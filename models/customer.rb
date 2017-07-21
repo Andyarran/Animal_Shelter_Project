28708c59b6ca50a401efafab85f94930e3f5a6ae
Andrew Weir <aweir@europe.com>
2017-07-21 13:38:10 +0100

    Added class functions to Customer class - delete all, find customer by id & display all customers

--- models/customer.rb
+++ models/customer.rb
@@ -2,7 +2,7 @@ require_relative( '../db/sql_runner' )
 
 class Customer
 
-attr_reader()
+attr_reader( :id, :first_name, :last_name, :address, :telephone)
 
   def initialize(options)
       @id = options['id'].to_i if options['id']
@@ -22,26 +22,49 @@ attr_reader()
       @id = customer_details['id'].to_i
   end
 
-
-  def find_animal()
-      sql = "SELECT * FROM animals WHERE id = #{@animal_id}"
-      animal = SqlRunner.run(sql).first
-      result = Animal.new( animal )
-      return result
-  end
-
   def update()
       sql = " UPDATE customers SET
       (first_name, last_name, address, telephone)
       VALUES
       ('#{@first_name}', '#{@last_name}', '#{@address}', '#{@telephone}')
-      WHERE id = #{@id} "
+      WHERE id = #{@id}; "
       SqlRunner.run(sql)
   end
 
+  def delete()
+      sql = " DELETE FROM customers WHERE id = #{@id};"
+      SqlRunner.run(sql)
+  end
+
+  def find_animal()
+      sql = "SELECT * FROM animals WHERE id = #{@animal_id};"
+      animal = SqlRunner.run(sql).first
+      result = Animal.new( animal )
+      return result
+  end
 
 
+  ########################### <- Class Functions Below -> ##################################
 
+
+  def self.delete_all()
+      sql = " DELETE FROM customers; "
+      SqlRunner.run(sql)
+  end
+
+  def self.all()
+      sql = " SELECT * FROM customers; "
+      customer_all = SqlRunner.run(sql)
+      customers = customer_all.map { |customer| Customer.new(customer) }
+      return customers
+  end
+
+  def self.find(id)
+      sql = " SELECT * FROM customers WHERE id = #{@id}; "
+      customers = SqlRunner.run(sql).first
+      result = customers.map { |customer| Customer.new(customer)}
+      return result
+  end
 
 
 
